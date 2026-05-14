9d4a48a6ad9635ea75538e11996dc07e7e3766dd
LeonardoFCorrea <leonardofelixcorrea@gmail.com>
2026-05-14 15:58:37 -0300

    added Try&Except Learning

---
+++ Try&Except.py
@@ -0,0 +1,32 @@
+carros = ["Gol", "Uno", "Civic"]
+
+try:
+    print(carros[3])
+except IndexError:
+    print("O índice está fora do alcance da lista.")
+try:
+    resultado = 10 / 0
+except ZeroDivisionError:
+    print("Não é possível dividir por zero.")
+try:
+    numero = int("abc")
+except ValueError:
+    print("Não é possível converter a string para um número inteiro.")
+try:    arquivo = open("arquivo_inexistente.txt", "r")
+except FileNotFoundError:
+    print("O arquivo não foi encontrado.")
+    
+try:
+    numero = int(input("Digite um número: "))
+except ValueError:
+    print("Entrada inválida. Por favor, digite um número inteiro.")
+    
+user_input = input("Digite um número para dividir por 2: ")
+try:
+    result = int(user_input) / 2
+except:
+    print("Something went wrong.")
+else:
+    print(f"O resultado da divisão é: {result}")
+finally:
+    print("Este bloco será executado independentemente de ocorrer uma exceção ou não.")
