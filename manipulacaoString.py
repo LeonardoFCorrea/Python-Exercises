acd7443adbb77f3c89b72eb33125bec99cdfbabe
LeonardoFCorrea <leonardofelixcorrea@gmail.com>
2026-05-18 00:56:25 -0300

    Add manipulacaoString Finished Tests

---
+++ manipulacaoString.py
@@ -0,0 +1,12 @@
+string1 = 'Dog'
+
+char1 = string1[-2]
+print(char1)
+
+string2 = '          Cat Scratch and Meow '
+list_str_separated = string2.split()
+
+print(list_str_separated)
+print(string2.strip())
+
+print('C' in string2)
